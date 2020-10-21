cb0c042f1c717549cc2c9cb233681063e6552949
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-21 20:49:49 +0800

    TwoNumberSum

---
+++ AlgorithmExercises/TwoNumberSum.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmExercises
+{
+    class TwoNumberSum
+    {
+        private static int[] SolveA(int[] array, int targetSum)
+        {
+            // O(n) time | O(n) space
+
+            var numbers = new HashSet<int>();
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                var currentValue = array[i];
+                var requiredNumber = targetSum - currentValue;
+
+                if (numbers.Contains(requiredNumber))
+                {
+                    return new int[] { currentValue, requiredNumber };
+                }
+                else
+                {
+                    numbers.Add(currentValue);
+                }
+            }
+
+            return new int[] { };
+        }
+
+        private static int[] SolveB(int[] array, int targetSum)
+        {
+            // O(n) time | O(1) space
+
+            Array.Sort(array);
+
+            var leftIndex = 0;
+            var rightIndex = array.Length - 1;
+
+            while (leftIndex < rightIndex)
+            {
+                var leftValue = array[leftIndex];
+                var rightValue = array[rightIndex];
+                var currentSum = leftValue + rightValue;
+
+                if (currentSum == targetSum)
+                {
+                    return new int[] { leftValue, rightValue };
+                }
+                else if (currentSum > targetSum)
+                {
+                    rightIndex--;
+                }
+                else if (currentSum < targetSum)
+                {
+                    leftIndex++;
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
