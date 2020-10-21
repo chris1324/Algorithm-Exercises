1f249cd24c771790ee88e8f531fe08a709beb6cf
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-21 18:54:02 +0800

    Initial Commit

---
+++ AlgorithmExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
