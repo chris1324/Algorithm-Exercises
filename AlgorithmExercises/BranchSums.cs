89356a81ab019050713e1e3130beaf351053fa74
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-22 22:16:11 +0800

    BranchSums

---
+++ AlgorithmExercises/BranchSums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmExercises
+{
+    class BranchSums
+    {
+        public class BinaryTree
+        {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+                this.left = null;
+                this.right = null;
+            }
+        }
+
+        public static List<int> Solve(BinaryTree root)
+        {
+            // O(n) time | O(n) space
+            return CalculateBranchSum(root, 0);
+        }
+
+        private static List<int> CalculateBranchSum(BinaryTree node, int currentSum)
+        {
+            currentSum += node.value;
+
+            var results = new List<int> { };
+            var isLeaf = node.left == null && node.right == null;
+
+            if (isLeaf)
+            {
+                results.Add(currentSum);
+            }
+            else
+            {
+                if (node.left != null) results = results.Concat(CalculateBranchSum(node.left, currentSum)).ToList();
+                if (node.right != null) results = results.Concat(CalculateBranchSum(node.right, currentSum)).ToList();
+            }
+
+            return results;
+        }
+    }
+}
