c61247a5924b7b10b56b0b8c81e98f14a7393d69
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-23 18:39:54 +0800

    InsertionSort

---
+++ AlgorithmExercises/InsertionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class InsertionSort
+    {
+        public static int[] Solve(int[] array)
+        {
+            // O(n^2) time | O(1) space
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                var isSorted = false;
+                var index = 1;
+                var previousIndex = i - 1;
+
+                while (previousIndex >= 0 || !isSorted)
+                {
+                    if (array[previousIndex] > array[index])
+                    {
+                        Swap(array, index, previousIndex);
+                        index--;
+                        previousIndex--;
+                    }
+                    else
+                    {
+                        isSorted = true;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        private static void Swap(int[] array, int indexA, int indexB)
+        {
+            var indexAValue = array[indexA];
+            array[indexA] = array[indexB];
+            array[indexB] = indexAValue;
+        }
+    }
+}
