4746fc3e6197cf62857e9800529d30b29662d208
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-23 23:30:51 +0800

    PalindromeCheck

---
+++ AlgorithmExercises/PalindromeCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace AlgorithmExercises
+{
+    class PalindromeCheck
+    {
+        public static bool SolveA(string str)
+        {
+            // O(n) time | O(1) space
+            // Iterative approach
+            var leftIndex = 0;
+            var rightIndex = str.Length - 1;
+
+            while (leftIndex < rightIndex)
+            {
+                if (str[leftIndex] != str[rightIndex]) return false;
+
+                leftIndex++;
+                rightIndex--;
+            }
+
+            return true;
+        }
+
+        public static bool SolveB(string str, int i = 0)
+        {
+            // O(n) time | O(n) space
+            // Recursive approach
+            var j = str.Length - 1 - i;
+
+            return i >= j ? true : str[i] == str[j] && SolveB(str, i + 1);
+        }
+    }
+}
