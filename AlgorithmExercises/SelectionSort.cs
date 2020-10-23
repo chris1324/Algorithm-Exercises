c89b903896aa4db31d9ba83e8763590f74751794
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-23 19:10:12 +0800

    SelectionSort

---
+++ AlgorithmExercises/SelectionSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.Json;
+
+namespace AlgorithmExercises
+{
+    class SelectionSort
+    {
+        public static void QuickTest()
+        {
+            var input = new int[] { 8, 5, 2, 9, 5, 6, 3 };
+
+            var result = Solve(input);
+
+            Console.WriteLine(JsonSerializer.Serialize(result));
+        }
+
+        public static int[] Solve(int[] array)
+        {
+            // O(n^2) time | O(1) space
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                var index = i;
+                var smallestValueIndex = i;
+
+                while (index < array.Length)
+                {
+                    var value = array[index];
+                    if (value < array[smallestValueIndex])
+                    {
+                        smallestValueIndex = index;
+                    }
+
+                    index++;
+                }
+
+                Swap(array, i, smallestValueIndex);
+            }
+
+            return array;
+        }
+
+        private static void Swap(int[] array, int indexA, int indexB)
+        {
+            var indexAValue = array[indexA];
+            array[indexA] = array[indexB];
+            array[indexB] = indexAValue;
+        }
+    }
+}
