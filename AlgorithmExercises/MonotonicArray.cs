dfd04eda12c56115e84b5c6f54c293c861f1e55a
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-25 19:03:09 +0800

    MonotonicArray

---
+++ AlgorithmExercises/MonotonicArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class MonotonicArray
+    {
+        static bool Solve(int[] array)
+        {
+            // O(n) time | O(1) space
+
+            var hasIncrease = false;
+            var hasDecrease = false;
+
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                if (hasIncrease && hasDecrease)
+                {
+                    break;
+                }
+
+                if (array[i] < array[i + 1])
+                {
+                    hasIncrease = true;
+                }
+                else if (array[i] > array[i + 1])
+                {
+                    hasDecrease = true;
+                }
+            }
+
+            return !(hasIncrease && hasDecrease);
+        }
+    }
+}
