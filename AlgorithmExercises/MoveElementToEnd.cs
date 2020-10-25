702f8deb0b6245fc89a3a5bcbfc4ab1c2a8283a5
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-25 19:03:14 +0800

    MoveElementToEnd

---
+++ AlgorithmExercises/MoveElementToEnd.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmExercises
+{
+    class MoveElementToEnd
+    {
+        static List<int> Solve(List<int> array, int toMove)
+        {
+            // O(n) time | O(1) space
+            var i = 0;
+            var j = array.Count - 1;
+
+            while (i < j)
+            {
+                if (array[j] == toMove)
+                {
+                    j--;
+                    continue;
+                }
+
+                if (array[i] == toMove)
+                {
+                    Swap(array, i, j);
+                }
+
+                i++;
+            }
+
+            return array;
+        }
+
+        private static void Swap(List<int> array, int indexA, int indexB)
+        {
+            var indexAValue = array[indexA];
+            array[indexA] = array[indexB];
+            array[indexB] = indexAValue;
+        }
+    }
+}
