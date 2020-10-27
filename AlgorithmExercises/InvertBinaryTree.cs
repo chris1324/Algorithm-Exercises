8711121cb79bcf760337c542c0600ad81058cac2
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-10-27 22:28:34 +0800

    InvertBinaryTree

---
+++ AlgorithmExercises/InvertBinaryTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class InvertBinaryTree
+    {
+        public static void Solve(BinaryTree tree)
+        {
+            // O(n) time | O(d) space - where n is the number of nodes and d is th depth (height)
+            tree.InvertTree();
+        }
+
+        public class BinaryTree
+        {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+            }
+
+            public void InvertTree()
+            {
+                var leftBinaryTree = left;
+                left = right;
+                right = leftBinaryTree;
+
+                if (left != null) left.InvertTree();
+                if (right != null) right.InvertTree();
+            }
+        }
+    }
+}
