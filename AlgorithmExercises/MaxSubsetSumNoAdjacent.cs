78e94324c6fe8d822d29ed4e3fafd130f00c229e
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-11-01 21:16:41 +0800

    MaxSubsetSumNoAdjacent

---
+++ AlgorithmExercises/MaxSubsetSumNoAdjacent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class MaxSubsetSumNoAdjacent
+    {
+        static int Solve(int[] array)
+        {
+            // O(n) time | O(i) space
+            if (array.Length == 0) return 0;
+            else if (array.Length == 1) return array[0];
+
+            var first = array[0];
+            var second = Math.Max(array[0], array[1]);
+
+            for (var i = 2; i < array.Length; i++)
+            {
+                var maxSum = Math.Max(second, first + array[i]);
+                first = second;
+                second = maxSum;
+            }
+
+            return second;
+        }
+    }
+}
