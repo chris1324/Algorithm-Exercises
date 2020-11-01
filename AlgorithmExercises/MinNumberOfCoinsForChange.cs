4515127b627fc69cf878726f688de54c2e8516d8
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-11-01 22:37:50 +0800

    MinNumberOfCoinsForChange

---
+++ AlgorithmExercises/MinNumberOfCoinsForChange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class MinNumberOfCoinsForChange
+    {
+        static int Solve(int n, int[] denoms)
+        {
+            // O(nd) time | O(n) space
+            var nums = new int[n + 1];
+            Array.Fill(nums, int.MaxValue);
+            nums[0] = 0;
+
+            foreach (var demon in denoms)
+            {
+                for (var amount = 1; amount <= n; amount++)
+                {
+                    if (amount < demon) continue;
+
+                    if (nums[amount - demon] == int.MaxValue) continue;
+
+                    nums[amount] = Math.Min(nums[amount], 1 + nums[amount - demon]);
+                }
+            }
+
+            return nums[n] == int.MaxValue ? -1 : nums[n];
+        }
+    }
+}
