06790c5bdf18ef6e4da285bf0d0c015f59f33efd
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-11-02 22:13:48 +0800

    KadaneAlgorithm

---
+++ AlgorithmExercises/KadaneAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class KadaneAlgorithm
+    {
+        static int Solve(int[] array)
+        {
+            var currentMaxSum = array[0];
+            var previousSum = array[0];
+
+            for (var i = 1; i < array.Length; i++)
+            {
+                var sum = Math.Max(previousSum + array[i], array[i]);
+
+                if (sum > currentMaxSum) currentMaxSum = sum;
+
+                previousSum = sum;
+            }
+
+            return currentMaxSum;
+        }
+    }
+}
