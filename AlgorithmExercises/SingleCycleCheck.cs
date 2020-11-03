a63abcdb25117f76dab904fd7944c3ea9670acea
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-11-03 17:30:50 +0800

    SingleCycleCheck

---
+++ AlgorithmExercises/SingleCycleCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class SingleCycleCheck
+    {
+        static bool Solve(int[] array)
+        {
+            // O(n) time | O(1) space
+            var numberOfJump = 0;
+            var currentIndex = 0;
+
+            while (numberOfJump < array.Length)
+            {
+                if (numberOfJump > 0 && currentIndex == 0) return false;
+
+                numberOfJump++;
+                currentIndex = GetNextIndex(currentIndex, array);
+            }
+
+            return currentIndex == 0;
+        }
+
+        private static int GetNextIndex(int currentIndex, int[] array)
+        {
+            var jump = array[currentIndex];
+            var nextIndex = (currentIndex + jump) % array.Length;
+
+            return nextIndex < 0 ? nextIndex + array.Length : nextIndex;
+        }
+    }
+}
