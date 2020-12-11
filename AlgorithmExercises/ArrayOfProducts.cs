66e789c4d9a6c7dad60692f93672ba0cc3eb9dfc
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-12-11 15:30:13 +0800

    ArrayOfProducts

---
+++ AlgorithmExercises/ArrayOfProducts.cs
@@ -0,0 +1,28 @@
+﻿namespace AlgorithmExercises
+{
+    class ArrayOfProducts
+    {
+        static int[] Solve(int[] array)
+        {
+            // O(n) time | O(n) space
+            var results = new int[array.Length];
+
+            var leftTotal = 1;
+            var rightTotal = 1;
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                results[i] = leftTotal;
+                leftTotal *= array[i];
+            }
+
+            for (var i = array.Length - 1; i >= 0; i--)
+            {
+                results[i] *= rightTotal;
+                rightTotal *= array[i];
+            }
+
+            return results;
+        }
+    }
+}
