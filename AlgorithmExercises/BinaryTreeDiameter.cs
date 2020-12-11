afebde06e8250f419fed3e4731b2cc389b61f727
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-12-11 16:16:00 +0800

    BinaryTreeDiameter

---
+++ AlgorithmExercises/BinaryTreeDiameter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AlgorithmExercises
+{
+    class BinaryTreeDiameter
+    {
+        public class BinaryTree
+        {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        public class TreeInfo
+        {
+            public int MaxLength { get; set; }
+            public int MaxCombinedLength { get; set; }
+        }
+
+        public int Solve(BinaryTree tree)
+        {
+            // O(n) time | O(h) space - where n is the number of nodes and h is the height of the Binary Tree
+            return GetTreeInfo(tree).MaxCombinedLength;
+        }
+
+        public TreeInfo GetTreeInfo(BinaryTree tree)
+        {
+            if (tree == null) return new TreeInfo { };
+
+            var leftTreeInfo = GetTreeInfo(tree.left);
+            var rightTreeInfo = GetTreeInfo(tree.right);
+
+            var mathLength = Math.Max(leftTreeInfo.MaxLength, rightTreeInfo.MaxLength) + 1;
+
+            var maxCombinedLength = 0;
+            maxCombinedLength = Math.Max(leftTreeInfo.MaxCombinedLength, rightTreeInfo.MaxCombinedLength);
+            maxCombinedLength = Math.Max(maxCombinedLength, leftTreeInfo.MaxLength + rightTreeInfo.MaxLength);
+
+            return new TreeInfo { MaxLength = mathLength, MaxCombinedLength = maxCombinedLength };
+        }
+    }
+}
