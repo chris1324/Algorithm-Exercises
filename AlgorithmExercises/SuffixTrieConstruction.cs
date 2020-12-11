07eac91fe29eb7b2bac99dd33197770f084ae78a
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-12-11 22:43:05 +0800

    SuffixTrieConstruction

---
+++ AlgorithmExercises/SuffixTrieConstruction.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmExercises
+{
+    class SuffixTrieConstruction
+    {
+        public static void QuickTest()
+        {
+            var trie = new SuffixTrie("babc");
+        }
+
+        public class TrieNode
+        {
+            public Dictionary<char, TrieNode> Children = new Dictionary<char, TrieNode>();
+        }
+
+        public class SuffixTrie
+        {
+            public TrieNode root = new TrieNode();
+            public char endSymbol = '*';
+
+            public SuffixTrie(string str)
+            {
+                PopulateSuffixTrieFrom(str);
+            }
+
+            public void PopulateSuffixTrieFrom(string str)
+            {
+                // O(n^2) time | O(n^2) space
+                for (var i = 0; i < str.Length; i++)
+                {
+                    var curNode = root;
+
+                    for (var j = i; j < str.Length; j++)
+                    {
+                        var letter = str[j];
+
+                        if (curNode.Children.ContainsKey(letter))
+                        {
+                            curNode = curNode.Children[letter];
+                        }
+                        else
+                        {
+                            var node = new TrieNode();
+                            curNode.Children.Add(letter, node);
+                            curNode = node;
+                        }
+                    }
+
+                    curNode.Children[endSymbol] = null;
+                }
+            }
+
+            public bool Contains(string str)
+            {
+                // O(n) time | O(1) space
+                var curNode = root;
+
+                for (var i = 0; i < str.Length; i++)
+                {
+                    var letter = str[i];
+
+                    if (!curNode.Children.ContainsKey(letter))
+                    {
+                        return false;
+                    }
+
+                    curNode = curNode.Children[letter];
+                }
+
+                return curNode.Children.ContainsKey(endSymbol);
+            }
+        }
+    }
+}
